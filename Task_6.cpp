a1787a6716d6cb7d527e93b5fd48c31bf23fab9c
Lispoihr <dmitriy.loznuha@gmail.com>
2025-09-26 01:01:53 +0300

    third option

--- Task_6.cpp
+++ Task_6.cpp
@@ -5,21 +5,30 @@ using namespace std;
 
 int main() {
     bool isIncorrect;
-    double x, p, K, D, C, B, A, Y;
+    double x, p, n, K, D, B, C, Q;
     do {
-        cout << "Введите занчение x, p, K, D, C: ";
-        cin >> x >> p >> K >> D >> C;
+        cout << "Введите значение x, p, n, K, D: ";
+        cin >> x >> p >> n >> K >> D;
         isIncorrect = false;
         if (cin.fail()) {
-            cout << "Ошибка ввода!!! Попробуйте еще раз!" << endl;
+            cout << "*Ошибка ввода!!! Попробуйте еще раз!" << endl;
             cin.clear();
             while(cin.get() != '\n');
             isIncorrect = true;
+            continue;
+        }
+        if (K == 0) {
+            cout << "*Ошибка: знаменатель равен нулю!!!" << '\n' << "Введите другое значение переменной K!" << endl;
+            isIncorrect = true;
+        }
+        if (D == 0) {
+            cout << "*Ошибка: знаменатель равен нулю!!!" << '\n' << "Введите другое значение переменной D!" << endl;
+            isIncorrect = true;
         }
     } while (isIncorrect);
-    A = x + sin(p);
-    B = exp(K);
-    Y = 1 + pow(K, 2) / 2 * A * B - B + D * C;
-    cout << "Соотношение, Вариант 2, Y = " << Y << endl;
+    C = p - n;
+    B = cos(x);
+    Q = pow(B, 2) / (K * D) + B * pow(C, 3);
+    cout << "Соотношение, Вариант 3, Q = " << Q << endl;
     return 0;
 }
