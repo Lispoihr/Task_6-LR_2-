b01fd2433af261915f776028274a4b37d880901c
Lispoihr <dmitriy.loznuha@gmail.com>
2025-09-27 21:19:28 +0300

    ninth option

--- Task_6.cpp
+++ Task_6.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main() {
     bool isIncorrect;
-    double x, d, C, K, A, B, Y;
+    double x, z, p, C, K, D, A, B, Y;
     do {
-        cout << "Введите значение x, d, C, K: ";
-        cin >> x >> d >> C >> K;
+        cout << "Введите значение x, z, p, C, K, D: ";
+        cin >> x >> z >> p >> C >> K >> D;
         isIncorrect = false;
         if (cin.fail()) {
             cout << "*Ошибка ввода!!! Попробуйте еще раз!" << endl;
@@ -17,18 +17,18 @@ int main() {
             isIncorrect = true;
             continue;
         }
-        if (K == 0) {
-            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение K! " << endl;
+        if (C == 0) {
+            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение C! " << endl;
             isIncorrect = true;
         }
-        if (x <= 0) {
-            cout << "*Ошибка!!! Логарифм с данным аргументом не существует!!!"<< '\n' << "Введите другое значение x! " << endl;
+        if (D == 0) {
+            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение D! " << endl;
             isIncorrect = true;
         }
     } while (isIncorrect);
-    A = log10(x);
-    B = x + exp(d);
-    Y = (A + B) - pow(C, 2) / K;
-    cout << "Соотношение, Вариант 8, Y = " << Y << endl;
+    A = sin(x) - z;
+    B = fabs(p - x);
+    Y = pow(A + B, 2) - K / (C * D);
+    cout << "Соотношение, Вариант 9, Y = " << Y << endl;
     return 0;
 }
