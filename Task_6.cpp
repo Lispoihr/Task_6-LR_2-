96a5ae2336d68faad31acb2a50ce3edf6849ab31
Lispoihr <dmitriy.loznuha@gmail.com>
2025-09-27 21:49:51 +0300

    tenth option

--- Task_6.cpp
+++ Task_6.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main() {
     bool isIncorrect;
-    double x, z, p, C, K, D, A, B, Y;
+    double x, k, z, C, D, A, B, Y;
     do {
-        cout << "Введите значение x, z, p, C, K, D: ";
-        cin >> x >> z >> p >> C >> K >> D;
+        cout << "Введите значение x, k, z, C, D: ";
+        cin >> x >> k >> z >> C >> D;
         isIncorrect = false;
         if (cin.fail()) {
             cout << "*Ошибка ввода!!! Попробуйте еще раз!" << endl;
@@ -17,18 +17,23 @@ int main() {
             isIncorrect = true;
             continue;
         }
-        if (C == 0) {
-            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение C! " << endl;
+        if (x <= 0) {
+            cout << "*Ошибка: логарифм с данным аргументом не существует!!!"<< '\n' << "Введите другое значение x! " << endl;
+            isIncorrect = true;
+            continue;
+        }
+        if (z < 0) {
+            cout << "*Ошибка: корень квадратный не находится из отрицательного числа!!!" << '\n' << "Введите другое значение z!" << endl;
             isIncorrect = true;
         }
-        if (D == 0) {
-            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение D! " << endl;
+        A = log(x) - k;
+        if (A == 0) {
+            cout << "*Ошибка: знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другие значения для x и(или) k! " << endl;
             isIncorrect = true;
         }
     } while (isIncorrect);
-    A = sin(x) - z;
-    B = fabs(p - x);
-    Y = pow(A + B, 2) - K / (C * D);
-    cout << "Соотношение, Вариант 9, Y = " << Y << endl;
+    B = sqrt(z);
+    Y = pow(D, 2) + pow(C, 2) / (0.75 * A) + B;
+    cout << "Соотношение, Вариант 10, Y = " << Y << endl;
     return 0;
 }
