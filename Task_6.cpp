f28474fda24b0be15279add553187f4a0863f9f4
Lispoihr <dmitriy.loznuha@gmail.com>
2025-09-27 12:39:31 +0300

    fourth option

--- Task_6.cpp
+++ Task_6.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main() {
     bool isIncorrect;
-    double x, p, n, K, D, B, C, Q;
+    double x, y, z, C, D, K, A, B, T;
     do {
-        cout << "Введите занчение x, p, n, K, D: ";
-        cin >> x >> p >> n >> K >> D;
+        cout << "Введите занчение x, y, z, C, D, K: ";
+        cin >> x >> y >> z >> C >> D >> K;
         isIncorrect = false;
         if (cin.fail()) {
             cout << "Ошибка ввода!!! Попробуйте еще раз!" << endl;
@@ -17,9 +17,9 @@ int main() {
             isIncorrect = true;
         }
     } while (isIncorrect);
-    C = p - n;
-    B = cos(x);
-    Q = pow(B, 2) / (K * D) + B * pow(C, 3);
-    cout << "Соотношение, Вариант 3, Q = " << Q << endl;
+    A = x - y;
+    B = sqrt(z);
+    T = cos(x) + pow(A, 2) / (K - C * D) - B;
+    cout << "Соотношение, Вариант 4, T = " << cos(x) << endl;
     return 0;
 }
