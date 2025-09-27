cbfbc6242d88c4bab47ac185fb22d64fc9c2999f
Lispoihr <dmitriy.loznuha@gmail.com>
2025-09-27 17:20:15 +0300

    sixth option

--- Task_6.cpp
+++ Task_6.cpp
@@ -5,21 +5,30 @@ using namespace std;
 
 int main() {
     bool isIncorrect;
-    double x, n, m, K, A, D, Y;
+    double x, y, C, K, A, D, S;
     do {
-        cout << "Введите занчение x, n, m, K: ";
-        cin >> x >> n >> m >> K;
+        cout << "Введите значение x, y, C, K: ";
+        cin >> x >> y >> C >> K;
         isIncorrect = false;
         if (cin.fail()) {
-            cout << "Ошибка ввода!!! Попробуйте еще раз!" << endl;
+            cout << "*Ошибка ввода!!! Попробуйте еще раз!" << endl;
             cin.clear();
             while(cin.get() != '\n');
             isIncorrect = true;
+            continue;
+        }
+        if (C == 0) {
+            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение C! " << endl;
+            isIncorrect = true;
+        }
+        if (K == 0) {
+            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение K! " << endl;
+            isIncorrect = true;
         }
     } while (isIncorrect);
-    A = fabs(n + m);
-    D = tan(x);
-    Y = 1.29 + K / A + pow(D, 2);
-    cout << "Соотношение, Вариант 5, Y = " << Y << endl;
+    A = x + y;
+    D = fabs(C - A);
+    S = 10.1 + A / C + D / pow(K, 2);
+    cout << "Соотношение, Вариант 6, S = " << S << endl;
     return 0;
 }
