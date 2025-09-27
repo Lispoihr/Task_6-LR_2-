e9511e20743bd9b68036cd53374db2f171f6643c
Lispoihr <dmitriy.loznuha@gmail.com>
2025-09-27 20:46:16 +0300

    seventh option

--- Task_6.cpp
+++ Task_6.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main() {
     bool isIncorrect;
-    double x, y, C, K, A, D, S;
+    double x, p, h, K, C, D, A, B, Y;
     do {
-        cout << "Введите значение x, y, C, K: ";
-        cin >> x >> y >> C >> K;
+        cout << "Введите значение x, p, h, K, C, D: ";
+        cin >> x >> p >> h >> K >> C >> D;
         isIncorrect = false;
         if (cin.fail()) {
             cout << "*Ошибка ввода!!! Попробуйте еще раз!" << endl;
@@ -17,18 +17,26 @@ int main() {
             isIncorrect = true;
             continue;
         }
-        if (C == 0) {
-            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение C! " << endl;
-            isIncorrect = true;
-        }
         if (K == 0) {
             cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение K! " << endl;
             isIncorrect = true;
         }
+        if (C == 0) {
+            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение C! " << endl;
+            isIncorrect = true;
+        }
+        if (D == 0) {
+            cout << "*Ошибка!!! Знаменатель не может быть равен нулю!!!"<< '\n' << "Введите другое значение D! " << endl;
+            isIncorrect = true;
+        }
+        if (h <= 0) {
+            cout << "*Ошибка!!! Логарифм с данным аргументом не существует!!!"<< '\n' << "Введите другое значение h! " << endl;
+            isIncorrect = true;
+        }
     } while (isIncorrect);
-    A = x + y;
-    D = fabs(C - A);
-    S = 10.1 + A / C + D / pow(K, 2);
-    cout << "Соотношение, Вариант 6, S = " << S << endl;
+    A = x - p;
+    B = log(h);
+    Y = 0.78 * B + pow(A, 3) /(K * C * D);
+    cout << "Соотношение, Вариант 7, Y = " << Y << endl;
     return 0;
 }
